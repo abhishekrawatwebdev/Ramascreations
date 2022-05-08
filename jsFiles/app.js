5d494468c0bb31bd383c904de1f0447593cc2c65
Abhishek rawat <98304693+abhishek-Capitall@users.noreply.github.com>
2022-05-08 13:30:54 +0530

    added responsiveness and active classes

--- jsFiles/app.js
+++ jsFiles/app.js
@@ -20,4 +20,64 @@ window.addEventListener('load', () => {
 
     });
 
+    const lgItems = document.getElementsByClassName("lg-list");
+    const lgLength = lgItems.length;
+
+    console.log(window.location.pathname);
+    let path = window.location.pathname;
+    if (path === "/Ramascreations/index.html") {
+        lgItems[0].classList.add("lgActive");
+    }
+    else if (path === "/Ramascreations/team.html") {
+        lgItems[1].classList.add("lgActive");
+
+    }
+    else if (path === "/Ramascreations/gallery.html") {
+        lgItems[2].classList.add("lgActive");
+
+    }
+    else if (path === "/Ramascreations/services.html") {
+        lgItems[3].classList.add("lgActive");
+
+    }
+    else if (path === "/Ramascreations/about.html") {
+        lgItems[4].classList.add("lgActive");
+    }
+    else if (path === "/Ramascreations/contact.html") {
+        lgItems[5].classList.add("lgActive");
+
+    }
+
+
+
+    const smItems = document.getElementsByClassName("sm-lists");
+    const smLength = smItems.length;
+
+    console.log(window.location.pathname);
+
+    if (path === "/Ramascreations/index.html") {
+        smItems[0].classList.add("smActive");
+    }
+    else if (path === "/Ramascreations/team.html") {
+        smItems[1].classList.add("smActive");
+
+    }
+    else if (path === "/Ramascreations/gallery.html") {
+        smItems[2].classList.add("smActive");
+
+    }
+    else if (path === "/Ramascreations/services.html") {
+        smItems[3].classList.add("smActive");
+
+    }
+    else if (path === "/Ramascreations/about.html") {
+        smItems[4].classList.add("smActive");
+    }
+    else if (path === "/Ramascreations/contact.html") {
+        smItems[5].classList.add("smActive");
+
+    }
+
+
+
 });
