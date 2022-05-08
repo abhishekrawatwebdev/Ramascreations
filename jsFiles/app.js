cb5498a2a3309eb00eaf8f5ebedf0749c52176d2
Abhishek rawat <98304693+abhishek-Capitall@users.noreply.github.com>
2022-05-08 13:40:05 +0530

    some js

--- jsFiles/app.js
+++ jsFiles/app.js
@@ -1,83 +1,81 @@
-window.addEventListener('load', () => {
-    console.log("gjdjkdc")
-    function myfunc() {
-        const see = document.getElementsByClassName("see")
-        const lets = document.getElementsByClassName("lets");
-        const animation = document.getElementById("animation");
-
-        animation.style.transform = 'translate(110vw,0VW)';
-
-    };
-
-    document.getElementById("openMenu").addEventListener('click', () => {
-
-        document.getElementById('nav-sm').style.right = "0%";
-
-    });
-    document.getElementById("menuClose").addEventListener('click', () => {
-
-        document.getElementById('nav-sm').style.right = "-100%";
-
-    });
-
-    const lgItems = document.getElementsByClassName("lg-list");
-    const lgLength = lgItems.length;
-
-    console.log(window.location.pathname);
-    let path = window.location.pathname;
-    if (path === "/Ramascreations/index.html") {
-        lgItems[0].classList.add("lgActive");
-    }
-    else if (path === "/Ramascreations/team.html") {
-        lgItems[1].classList.add("lgActive");
-
-    }
-    else if (path === "/Ramascreations/gallery.html") {
-        lgItems[2].classList.add("lgActive");
-
-    }
-    else if (path === "/Ramascreations/services.html") {
-        lgItems[3].classList.add("lgActive");
-
-    }
-    else if (path === "/Ramascreations/about.html") {
-        lgItems[4].classList.add("lgActive");
-    }
-    else if (path === "/Ramascreations/contact.html") {
-        lgItems[5].classList.add("lgActive");
-
-    }
 
 
+function myfunc() {
+    const see = document.getElementsByClassName("see")
+    const lets = document.getElementsByClassName("lets");
+    const animation = document.getElementById("animation");
 
-    const smItems = document.getElementsByClassName("sm-lists");
-    const smLength = smItems.length;
+    animation.style.transform = 'translate(110vw,0VW)';
 
-    console.log(window.location.pathname);
+};
 
-    if (path === "/Ramascreations/index.html") {
-        smItems[0].classList.add("smActive");
-    }
-    else if (path === "/Ramascreations/team.html") {
-        smItems[1].classList.add("smActive");
+document.getElementById("openMenu").addEventListener('click', () => {
 
-    }
-    else if (path === "/Ramascreations/gallery.html") {
-        smItems[2].classList.add("smActive");
-
-    }
-    else if (path === "/Ramascreations/services.html") {
-        smItems[3].classList.add("smActive");
-
-    }
-    else if (path === "/Ramascreations/about.html") {
-        smItems[4].classList.add("smActive");
-    }
-    else if (path === "/Ramascreations/contact.html") {
-        smItems[5].classList.add("smActive");
-
-    }
-
-
+    document.getElementById('nav-sm').style.right = "0%";
 
 });
+document.getElementById("menuClose").addEventListener('click', () => {
+
+    document.getElementById('nav-sm').style.right = "-100%";
+
+});
+
+const lgItems = document.getElementsByClassName("lg-list");
+const lgLength = lgItems.length;
+
+
+let path = window.location.pathname;
+if (path === "/Ramascreations/index.html") {
+    lgItems[0].classList.add("lgActive");
+}
+else if (path === "/Ramascreations/team.html") {
+    lgItems[1].classList.add("lgActive");
+
+}
+else if (path === "/Ramascreations/gallery.html") {
+    lgItems[2].classList.add("lgActive");
+
+}
+else if (path === "/Ramascreations/services.html") {
+    lgItems[3].classList.add("lgActive");
+
+}
+else if (path === "/Ramascreations/about.html") {
+    lgItems[4].classList.add("lgActive");
+}
+else if (path === "/Ramascreations/contact.html") {
+    lgItems[5].classList.add("lgActive");
+
+}
+
+
+
+const smItems = document.getElementsByClassName("sm-lists");
+const smLength = smItems.length;
+
+
+
+if (path === "/Ramascreations/index.html") {
+    smItems[0].classList.add("smActive");
+}
+else if (path === "/Ramascreations/team.html") {
+    smItems[1].classList.add("smActive");
+
+}
+else if (path === "/Ramascreations/gallery.html") {
+    smItems[2].classList.add("smActive");
+
+}
+else if (path === "/Ramascreations/services.html") {
+    smItems[3].classList.add("smActive");
+
+}
+else if (path === "/Ramascreations/about.html") {
+    smItems[4].classList.add("smActive");
+}
+else if (path === "/Ramascreations/contact.html") {
+    smItems[5].classList.add("smActive");
+
+}
+
+
